49ebd2b35757bf450a325e72b85d94f008c09697
Ivan <ivan_921225@outlook.com>
2018-03-29 20:20:23 -0500

    Creando bottonBar

---
+++ app/src/main/java/com/ivan/platzigram/view/ContainerActivity.java
@@ -0,0 +1,20 @@
+package com.ivan.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ivan.platzigram.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+
+
+
+    }
+
+
+}
