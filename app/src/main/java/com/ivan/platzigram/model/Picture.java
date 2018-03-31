1abbf123f4a109d9702cc091899ac3233cef30cb
Ivan <ivan_921225@outlook.com>
2018-03-31 05:14:05 -0500

    Antesde comenzar el RecyclerView

---
+++ app/src/main/java/com/ivan/platzigram/model/Picture.java
@@ -0,0 +1,52 @@
+package com.ivan.platzigram.model;
+
+/**
+ * Created by Ivan on 31/03/2018.
+ */
+
+public class Picture {
+
+    private String picture;
+    private String username;
+    private String time;
+    private String like_number="0";
+
+    public Picture(String picture, String username, String time, String like_number) {
+        this.picture = picture;
+        this.username = username;
+        this.time = time;
+        this.like_number = like_number;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLike_number() {
+        return like_number;
+    }
+
+    public void setLike_number(String like_number) {
+        this.like_number = like_number;
+    }
+}
--- app/src/main/java/com/ivan/platzigram/view/ContainerActivity.java
+++ app/src/main/java/com/ivan/platzigram/view/ContainerActivity.java
@@ -1,20 +1,56 @@
 package com.ivan.platzigram.view;
 
+//import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ivan.platzigram.R;
+import com.ivan.platzigram.view.fragments.HomeFragment;
+import com.ivan.platzigram.view.fragments.ProfileFragment;
+import com.ivan.platzigram.view.fragments.SearchFragment;
 
 public class ContainerActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.home:
+                    transaction.replace(R.id.container,new HomeFragment()).commit();
+                    return true;
+                case R.id.profile:
+                    transaction.replace(R.id.container,new ProfileFragment()).commit();
+                    return true;
+                case R.id.search:
+                    transaction.replace(R.id.container,new SearchFragment())
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottombar);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
 
 
     }
-
 
 }
